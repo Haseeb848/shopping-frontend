1b9cc5a80f3b26121f91c071ed8aa7dd6a200369
Haseeb848 <haseebamperortech.com>
2023-05-23 14:15:50 +0500

    added interface types

--- src/component/types.ts
+++ src/component/types.ts
@@ -4,3 +4,29 @@ export interface SignUpFormData {
   firstName: string;
   lastName: string;
 }
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  createdByCustomerId: number;
+  createdByRole: string;
+}
+
+export interface CartItem {
+  id: number;
+  productId: number;
+  quantity: number;
+  cartId: number;
+  product: Product; // assuming each item has a 'product' object with product details
+}
+
+export interface Cart {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  customerId: number;
+  items: CartItem[];
+  totalPrice: number;
+}
